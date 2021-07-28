0a05c52a931cb448c742ac7e0b469495958ed090
CyrusWarner <cyruswarner27@gmail.com>
2021-07-28 13:30:45 -0500

    updated flashcards urls.py.

--- flashcardproject/flashcard/urls.py
+++ flashcardproject/flashcard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('flashcard/post_flashcard', views.FlashcardPost.as_view()),
+    path('flashcard/post_flashcard/', views.FlashcardPost.as_view()),
     path('flashcard/<int:flashcard_id>/', views.FlashcardUpdate.as_view()),
     path('collections/<int:collection_id>/', views.FlashcardList.as_view()),
     path('flashcard_delete/<int:flashcard_id>/', views.FlashcardDelete.as_view()),
