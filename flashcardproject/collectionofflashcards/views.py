a41baa8aef8c2236764336b193533e249dd11d96
CyrusWarner <cyruswarner27@gmail.com>
2021-07-28 10:37:23 -0500

    Created model for collection of flashcards.

--- flashcardproject/collectionofflashcards/views.py
+++ flashcardproject/collectionofflashcards/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+
+"""
+from django.http import Http404
+from .models import Replies
+from .serializers import RepliesSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.apps import apps
+from django.db.models import Q
+"""
 
 # Create your views here.
